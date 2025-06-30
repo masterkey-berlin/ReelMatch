5123bd05e200d17de385d302a330114588d221a0
masterkey24 <manuel.huck@edu.techstarter.de>
2025-06-30 13:11:04 +0200

    Created upload_function and installed middleware multer

---
+++ backend/src/middleware/upload.middleware.js
@@ -0,0 +1,34 @@
+import multer from 'multer';
+import path from 'path';
+
+// Konfiguriere den Speicherort und den Dateinamen
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Stelle sicher, dass der Ordner 'uploads/' existiert
+  },
+  filename: function (req, file, cb) {
+    // Erstelle einen eindeutigen Dateinamen, um Kollisionen zu vermeiden
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+// Filter für Dateitypen (nur Videos)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('video/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Not a video file!'), false);
+  }
+};
+
+// Multer-Instanz mit Konfiguration
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 30 // 30 MB Limit
+  },
+  fileFilter: fileFilter
+});
+
+export default upload;
