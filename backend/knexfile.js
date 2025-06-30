8cbffd7f294a3337081ab0d6dd487c9180aacd7d
masterkey24 <manuel.huck@edu.techstarter.de>
2025-06-30 11:30:36 +0200

    Integration of the migration tool knex, creation of the knexconfig file and modification of the .env file with database credentials

---
+++ backend/knexfile.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'; // Lädt die .env Datei
+
+export default {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations'
+    }
+  },
+};
